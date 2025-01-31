9a64310dc77c626b02bb47b74158f148fe73dcd7
ttoonn112 <ttoonn112@gmail.com>
2025-01-31 18:59:11 +0700

    Update NavModule.java

--- android/src/main/java/com/google/android/react/navsdk/NavModule.java
+++ android/src/main/java/com/google/android/react/navsdk/NavModule.java
@@ -54,7 +54,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
-import com.google.android.libraries.navigation.CustomRoutesOptions;  //Kiattichai
+import com.google.android.libraries.navigation.CustomRoutesOptions;   //Kiattichai
+import com.google.android.libraries.navigation.TravelMode;            //Kiattichai
 
 /**
  * This exposes a series of methods that can be called diretly from the React Native code. They have
